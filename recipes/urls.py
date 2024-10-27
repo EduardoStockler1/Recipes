16a3e055deac58f547ccde3f05cd2819cb757fdb
Eduardo Stockler <stocklerdudu@gmail.com>
2024-10-27 20:45:28 -0300

    Mudanças no view e no urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path, include 
-from recipes.views import home, about, contact, login
+from recipes.views import home
 
 #dominio.com/recipes/
 urlpatterns = [
     path('', home),#Home
-    path('About', about),#About
-    path('contact', contact),#Contact
-    path('login', login)#Login
-
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 #HTTP Request <- HTTP Response
@@ -9,15 +9,3 @@ from django.http import HttpResponse
 def home(request):
     ...
     return render(request, 'recipes/home.html')
-
-def about(request):
-    ...
-    return render(request, 'recipes/about.html')
-
-def contact(request):
-    ...
-    return render(request, 'recipes/contact.html')
-
-def login(request):
-    ...
-    return render(request, 'recipes/login.html')
