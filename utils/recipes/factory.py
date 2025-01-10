6bf1e5381dd5200d2feba5c40dc7dc4bf2ff6ae1
Eduardo Stockler <stocklerdudu@gmail.com>
2025-01-10 17:46:33 -0300

    criando models e corrigindo detalhes do css

--- utils/recipes/factory.py
+++ utils/recipes/factory.py
@@ -12,13 +12,14 @@ fake = Faker('pt-BR')
 
 def make_recipe():
     return{
+        'id': fake.random_number(fix_len=True),
         'title': fake.sentence(nb_words=6),
         'description': fake.sentence(nb_words=12),
         'preparation_time': fake.random_number(digits=2, fix_len=True),
         'preparation_time_unit': 'Minutos',
         'servings': fake.random_number(digits=2, fix_len=True),
         'servings_unit': 'Porção',
-        'prepararion_steps': fake.text(3000),
+        'preparation_steps': fake.text(3000),
         'created_at': fake.date_time(),
         'author':{
             'first_name': fake.first_name(),
@@ -27,9 +28,10 @@ def make_recipe():
         'category':{
             'name': fake.word(),
         },
-        'cover':{
-            'url': 'https://loremflickr.com/%s/%s/food,cook' % rand_ratio(),
-        }
+        'cover': {
+            'url': 'https://loremflickr.com/%d/%d/' % rand_ratio(),
+        },
+
     }
 
 if __name__ == '__main__':
